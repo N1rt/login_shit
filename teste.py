ec084dcfae731cb476452c102f4f5852dc6ab1d1
N1rt <129163159+N1rt@users.noreply.github.com>
2023-03-28 18:23:28 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,19 +1,25 @@
 menu = (1,2,3,4)
 menu_anwser = int(input('oque gostaria de fazer? \n [1] fazer login \n [2] adicionar usuario \n [3] remover usuario \n [4] sair \n:'.capitalize()))
 #abrindo a lista
-lista = open('the path to the text file here','r')
+lista = open('C:/Users/hyand/Documents/python_projects/texto.txt','r')
 #passando o atributo object pra a var user_list
 users_list = lista
 #transformando-a em string e lendo ela
 user_list_to_string = str(users_list.read())
-#verificando oque o usuario quer fazer
+
 if(menu_anwser == menu[0]):
 	#esperando o resultado do input do nome do usuario
 	user = input("digite seu nome:".capitalize())
 	#checando se o nome do usuario é valido e existe na lista
+	if(user == ""):
+		print('digite um usuario valido!')
+		exit()
 	if(user_list_to_string.find(user) != -1):
 	#se for valido prosseguir para a senha,caso contrario exibir 'usuario inexistente'
 		password = input('digite sua senha:'.capitalize())
+		if(password == ""):
+			print('digite uma senha valida!')
+			exit()
 	#se a senha estiver na lista e ao lado do nome do usuario,permitir o acesso
 		if(user_list_to_string.find(f'{user}:{password}') != -1):
 			print(f'acesso concedido,bem vindo {user}'.capitalize())
@@ -22,14 +28,12 @@ if(menu_anwser == menu[0]):
 	else:
 		print('usuario inexistente!'.capitalize()) 
 elif(menu_anwser == menu[1]):
-	#adicionando um novo usuario
 	new_user = str(input('digite seu novo usuario assim:"usuario:senha"'.capitalize()))
-	lista_edit = open("the path to the text file here",'w')
+	lista_edit = open("C:/Users/hyand/Documents/python_projects/texto.txt",'w')
 	lista_edit.write(f'{user_list_to_string}\n{new_user}')
 elif(menu_anwser == menu[2]):
-	#deletando um usuario
 	delete_user = str(input('digite "nome:senha" do seu usuario para apaga-lo:'))
-	lista_edit = open("the path to the text file here",'w')
+	lista_edit = open("C:/Users/hyand/Documents/python_projects/texto.txt",'w')
 	lista_edit.write(f'{user_list_to_string.replace(delete_user,"")}')
 if(menu_anwser == menu[3]):
 	exit()
