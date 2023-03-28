ee9dd242f16dabdde6eea7789b5b45988c9ce682
N1rt <129163159+N1rt@users.noreply.github.com>
2023-03-28 06:25:29 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,33 @@
+menu = (1,2,3,4)
+menu_anwser = int(input('oque gostaria de fazer? \n [1] fazer login \n [2] adicionar usuario \n [3] remover usuario \n [4] sair \n:'.capitalize()))
+#abrindo a lista
+lista = open('C:/Users/hyand/Documents/python_projects/texto.txt','r')
+#passando o atributo object pra a var user_list
+users_list = lista
+#transformando-a em string e lendo ela
+user_list_to_string = str(users_list.read())
+
+if(menu_anwser == menu[0]):
+	#esperando o resultado do input do nome do usuario
+	user = input("digite seu nome:".capitalize())
+	#checando se o nome do usuario é valido e existe na lista
+	if(user_list_to_string.find(user) != -1):
+	#se for valido prosseguir para a senha,caso contrario exibir 'usuario inexistente'
+		password = input('digite sua senha:'.capitalize())
+	#se a senha estiver na lista e ao lado do nome do usuario,permitir o acesso
+		if(user_list_to_string.find(f'{user}:{password}') != -1):
+			print(f'acesso concedido,bem vindo {user}'.capitalize())
+		else:
+			print('senha incorreta!'.capitalize())
+	else:
+		print('usuario inexistente!'.capitalize()) 
+elif(menu_anwser == menu[1]):
+	new_user = str(input('digite seu novo usuario assim:"usuario:senha"'.capitalize()))
+	lista_edit = open("C:/Users/hyand/Documents/python_projects/texto.txt",'w')
+	lista_edit.write(f'{user_list_to_string}\n{new_user}')
+elif(menu_anwser == menu[2]):
+	delete_user = str(input('digite "nome:senha" do seu usuario para apaga-lo:'))
+	lista_edit = open("C:/Users/hyand/Documents/python_projects/texto.txt",'w')
+	lista_edit.write(f'{user_list_to_string.replace(delete_user,"")}')
+if(menu_anwser == menu[3]):
+	exit()
